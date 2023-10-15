c927f5dfa65912501af0f06029478fee71d5d2d0
emirhan.kalem320 <emirhan.kalem320@gmail.com>
2023-10-15 17:09:00 +0300

    Add project files.

---
+++ RedisApp/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace RedisApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private IDistributedCache _distributedCache;
+
+        public ProductsController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RedisApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RedisApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
